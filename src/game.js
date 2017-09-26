4c7c43d0b330a22a7e07b4227668be71722bde90
Tinatinak <tina.kallio@gmail.com>
2017-09-26 13:20:01 +0200

    change Game to class

--- src/game.js
+++ src/game.js
@@ -2,25 +2,39 @@ import React from 'react'
 import Card from "./card"
 import Counter from "./counter"
 
-const Game = () => (
-  <div className="game">
-    <h1>Memory </h1>
-    <p>Pick two cards. If the image is the same, you have a pair! <br />
-    The game is won when all pairs have been found. </p>
-    <Counter />
-    <Card src="/images/dog1.jpg"/>
-    <Card src="/images/dog2.jpg"/>
-    <Card src="/images/dog3.jpg"/>
-    <Card src="/images/dog4.jpg"/>
-    <Card src="/images/dog5.jpg"/>
-    <Card src="/images/dog6.jpg"/>
-    <Card src="/images/dog1.jpg"/>
-    <Card src="/images/dog2.jpg"/>
-    <Card src="/images/dog3.jpg"/>
-    <Card src="/images/dog4.jpg"/>
-    <Card src="/images/dog5.jpg"/>
-    <Card src="/images/dog6.jpg"/>
-  </div>
-)
+
+class Game extends React.Component {
+
+//State needs to be an object!!
+  state = {
+    cards: [
+      {src: "/images/dog1.jpg"},
+      {src: "/images/dog2.jpg"},
+      {src: "/images/dog3.jpg"},
+      {src: "/images/dog4.jpg"},
+      {src: "/images/dog5.jpg"},
+      {src: "/images/dog6.jpg"}
+    ]
+  }
+
+  render () {
+    return (
+      <div className="game">
+        <h1>Memory </h1>
+        <p>Pick two cards. If the image is the same, you have a pair! <br />
+        The game is won when all pairs have been found. </p>
+
+        <Card src={this.state.cards[0].src} />
+        <Card src={this.state.cards[1].src} />
+        <Card src={this.state.cards[2].src} />
+
+
+      </div>
+    )
+  }
+
+}
+
+
 
 export default Game
